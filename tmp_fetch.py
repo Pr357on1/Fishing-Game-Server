2db4b0152be4fec91a138078948b2aefda1c660c
Pr357on <Pr357on1@gmail.com>
2026-01-15 18:01:01 -0800

    Initial commit

--- tmp_fetch.py
+++
@@ -1,5 +0,0 @@
-import urllib.request, re
-url = "https://opengameart.org/content/cute-fish-sprites"
-html = urllib.request.urlopen(url).read().decode('utf-8')
-match = re.search(r"class=\"submitted\".*?username\">([^<]+)<", html, re.S)
-print(match.group(1) if match else 'unknown')
